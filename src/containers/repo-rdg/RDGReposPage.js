53d5c589fa518ac402c5df06dad9dead55b37733
milkers <ilker.sarac@corp.reysastech.com>
2018-01-18 14:42:26 +0300

    first commit

---
+++ src/containers/repo-rdg/RDGReposPage.js
@@ -0,0 +1,129 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classNames from "classnames";
+
+import {
+  invalidateReposPage,
+  selectReposPage,
+  fetchTopReposIfNeeded
+} from "../../actions/repos";
+
+// import "react-virtualized/styles.css";
+import "./repo.css";
+
+class RDGReposPage extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleNextPageClick = this.handleNextPageClick.bind(this);
+    this.handlePreviousPageClick = this.handlePreviousPageClick.bind(this);
+    this.handleRefreshClick = this.handleRefreshClick.bind(this);
+    this.getNoRowsRenderer = this.getNoRowsRenderer.bind(this);
+    this.getRowClassName = this.getRowClassName.bind(this);
+  }
+
+  componentDidMount() {
+    const { dispatch, page } = this.props;
+    dispatch(fetchTopReposIfNeeded(page));
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { dispatch, page } = nextProps;
+    dispatch(fetchTopReposIfNeeded(page));
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return shallowCompare(this, nextProps, nextState);
+  // }
+
+  getNoRowsRenderer() {
+    return (
+      <div className="noRows">
+        No rows
+      </div>
+    );
+  }
+
+  getRowClassName({ index }) {
+    if (index < 0) {
+      return "headerRow";
+    }
+    return index % 2 === 0 ? "evenRow" : "oddRow";
+  }
+
+  handleNextPageClick(e) {
+    e.preventDefault();
+    const { page, repos } = this.props;
+    if (repos.length > 0) {
+      // go to next page only if more data may be available
+      this.props.dispatch(selectReposPage(page + 1));
+    }
+  }
+
+  handlePreviousPageClick(e) {
+    e.preventDefault();
+    const page = this.props.page;
+    if (page > 1) {
+      this.props.dispatch(selectReposPage(page - 1));
+    }
+  }
+
+  handleRefreshClick(e) {
+    e.preventDefault();
+
+    const { dispatch, page } = this.props;
+    dispatch(invalidateReposPage(page));
+  }
+
+
+
+  render() {
+    const { page, error, repos, isFetching } = this.props;
+    const prevStyles = classNames("page-item", { disabled: page <= 1 });
+    const nextStyles = classNames("page-item", {
+      disabled: repos.length === 0
+    });
+
+    return (
+      <div className="container">
+        This is react-data-grid  
+      </div>
+    );
+  }
+}
+
+RDGReposPage.propTypes = {
+  page: PropTypes.number.isRequired,
+  repos: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.object,
+  dispatch: PropTypes.func.isRequired,
+  left: PropTypes.number,
+  top: PropTypes.number
+};
+
+function mapStateToProps(state) {
+  const { selectedReposPage, reposByPage } = state;
+  const page = selectedReposPage || 1;
+  if (!reposByPage[page]) {
+    return {
+      page,
+      error: null,
+      isFetching: false,
+      didInvalidate: false,
+      totalCount: 0,
+      repos: []
+    };
+  }
+
+  return {
+    page,
+    error: reposByPage[page].error,
+    isFetching: reposByPage[page].isFetching,
+    didInvalidate: reposByPage[page].didInvalidate,
+    totalCount: reposByPage[page].totalCount,
+    repos: reposByPage[page].repos
+  };
+}
+
+export default connect(mapStateToProps)(RDGReposPage);
